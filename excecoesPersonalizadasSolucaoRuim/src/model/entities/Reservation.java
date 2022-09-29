8a49af687d455449cfe7677cb204adb75f104a8b
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-29 10:48:12 -0300

    solução ruim

--- excecoesPersonalizadasSolucaoRuim/src/model/entities/Reservation.java
+++ excecoesPersonalizadasSolucaoRuim/src/model/entities/Reservation.java
@@ -46,14 +46,22 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";
+		} 
+		if (!checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date";
+		} 
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		
+		return null;
 	}
 	@Override
 	public String toString() {
-		return "Room"
+		return "Room "
 				+ roomNumber
 				+ ", check-in: "
 				+sdf.format(checkIn)
@@ -61,7 +69,7 @@ public class Reservation {
 				+sdf.format(checkOut)
 				+","
 				+ duration()
-				+"nights";
+				+" nights";
 				
 		
 	}
